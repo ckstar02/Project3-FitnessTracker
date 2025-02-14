91e876912167a8d7e7fdd88d093a48f077657256
Ta'Lee Hollins-Bailey <taleeh98454@gmail.com>
2025-02-13 18:47:18 -0600

    removed Typescript and added some code for Header and landing page

---
+++ client/src/main.jsx
@@ -0,0 +1,49 @@
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App";
+import Error from "./pages/Error";
+import Home from "./components/Header";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />
+      },
+      {
+        path: 'landingpage',
+        element: <LandingPage />
+      },
+      {
+        path: 'error',
+        element: <Error />
+      },
+      {
+        path: 'home',
+        element: <Home />
+      },
+      {
+        path: 'login',
+        element: <Login />
+      },
+      {
+        path: 'signup',
+        element: <SignUp />
+      }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
