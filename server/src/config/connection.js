172a9aa17a8f8ba03e5162442c846c52d72fa31b
Nattan Aguia <nattan23@msn.com>
2025-02-18 19:44:49 -0500

    connection

--- server/src/config/connection.js
+++ server/src/config/connection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const db = async () => {
     try {
-        await mongoose.connect(process.env.ATLASURI || 'mongodb://127.0.0.1:27017/social_networkdb');
+        await mongoose.connect(process.env.ATLASURI || 'mongodb://127.0.0.1:27017/fitness_startDb');
         console.log('Database connected.');
         return mongoose.connection;
     } catch(error) {
