473f0b54e3e3601fa39c70fe829ff2f7cdc68b9b
Nattan Aguia <nattan23@msn.com>
2025-03-10 17:17:27 -0400

    bootstrap

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from "./App";
 import Error from "./pages/Error";
