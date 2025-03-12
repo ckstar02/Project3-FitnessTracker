8072c795196f8281e8306a9f67376db329019990
Mason Mueggenborg <masong.mug@gmail.com>
2025-03-12 18:08:56 -0500

    added searchRecipie function

--- client/src/pages/recipieSearch.jsx
+++ client/src/pages/recipieSearch.jsx
@@ -1,6 +1,7 @@
 import '../css/recipieSearch.css';
 import React from 'react';
 import Header from '../components/Header';
+import searchRecipie from '../components/searchRecipie'
 
 export default function recipieSearch(){
   return(
